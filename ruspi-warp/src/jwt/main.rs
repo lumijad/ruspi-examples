8a27bcd8e48ba8e6aca4957f64af73ba3f9370b4
Rolf <rolf@scherer.ml>
2020-05-13 15:46:31 +0200

    jwt example enhanced

--- ruspi-warp/src/jwt/main.rs
+++ ruspi-warp/src/jwt/main.rs
@@ -1,8 +1,8 @@
 use argon2::{self, Config};
-use rand::Rng;
+use chrono::{Duration, Utc};
 use jsonwebtoken::{decode, DecodingKey, encode, EncodingKey, errors, Header, Validation};
+use rand::Rng;
 use serde::{Deserialize, Serialize};
-use chrono::{Utc, Duration};
 
 pub fn hash(password: &[u8]) -> String {
     let salt = rand::thread_rng().gen::<[u8; 32]>();
@@ -13,8 +13,6 @@ pub fn hash(password: &[u8]) -> String {
 pub fn verify(hash: &str, password: &[u8]) -> bool {
     argon2::verify_encoded(hash, password).unwrap_or(false)
 }
-
-pub const TOKEN_PREFIX: &str = "Bearer ";
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Claims {
@@ -48,13 +46,41 @@ pub fn encode_token(secret: &str, sub: String, auth: Vec<String>) -> String {
 
 pub fn decode_token(secret: &str, token: &str) -> errors::Result<Claims> {
     decode::<Claims>(
-        token.trim_start_matches(TOKEN_PREFIX),
+        token,
         &DecodingKey::from_secret(secret.as_ref()),
         &Validation::default(),
     ).map(|token_data| token_data.claims)
 }
 
+pub fn login(username: String, password: String, jwt_secret: String) -> String {
+    let hashed_password = "$argon2i$v=19$m=4096,t=3,p=1$wBNL0yEpl0+lx5jp28bwsYwQVJ5oMBVm8Yi5WFXtguo$Cz0CHGSf8rzg9y/Rq6pMOzT5o82UZqIixyPyKf2R54A";
+
+    if username != "admin".to_string() {
+        panic!("Invalid username");
+    }
+
+    if !verify(hashed_password, password.as_bytes()) {
+        panic!("Invalid password");
+    }
+
+    let rights = vec!["admin".to_string(), "user".to_string()];
+    let token = encode_token(&jwt_secret, username, rights);
+
+    token
+}
 
 
 fn main() {
+    let jwt_secret = String::from("d302886ecca83222a392f6549e520dacbd95dc65f1a303f8c571ca3bb9b8e196");
+
+    let username = String::from("admin");
+    let password = String::from("Admin!");
+
+    let token = login(username, password, jwt_secret.clone());
+
+    println!("Token: {}", token);
+
+    let claims = decode_token(&jwt_secret, &token).unwrap();
+
+    println!("Claims: {:?}", claims);
 }
